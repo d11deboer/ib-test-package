4524cd14b86ef070afa1f682e7f56a96095a8a48
David D <46694467+d11deboer@users.noreply.github.com>
2021-10-13 14:40:04 -0600

    Update and rename test.lua to node.lua

---
+++ node.lua
@@ -0,0 +1,13 @@
+gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
+
+local font = resource.load_font "./quicksand/Quicksand-Regular.otf"
+local text = "LETS GOOOO"
+
+util.json_watch("config.json", function(config)
+    text = config.text -- update text via config
+end)
+
+function node.render()
+    gl.clear(0,0,0,1)
+    font:write(250, 300, text, 64, 1,1,1,1) -- use the CONFIG value
+end
--- test.lua
+++
@@ -1,7 +0,0 @@
-gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
-
-local font = resource.load_font "./quicksand/Quicksand-Regular.otf"
-
-function node.render()
-    font:write(250, 300, "Hello world", 64, 1,1,1,1)
-end
