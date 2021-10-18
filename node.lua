9f291ac40703f67d4a2ad837ca44c47a5b25fc11
David D <46694467+d11deboer@users.noreply.github.com>
2021-10-18 16:32:58 -0600

    test GET

--- node.lua
+++ node.lua
@@ -2,8 +2,11 @@ gl.setup(NATIVE_WIDTH, 768)
 
 local font = resource.load_font("Pacifico.ttf")
 local background = resource.load_image("zoid.jpeg")
+local http = require("socket.http")
+local body, code, headers, status = http.request("https://www.google.com")
+
 
 function node.render()
     background:draw(0, 0, WIDTH, HEIGHT)
-    font:write(120, 320, "Hello World", 100, 1,1,1,1)
+    font:write(120, 320, status, 100, 1,1,1,1)
 end
