e06dcbd62c3edae3b943e3af246ca0a8edd75cb0
David D <46694467+d11deboer@users.noreply.github.com>
2021-10-18 11:08:09 -0600

    Adding background

--- node.lua
+++ node.lua
@@ -1,7 +1,9 @@
-gl.setup(1024, 768)
+gl.setup(NATIVE_WIDTH, 768)
 
 local font = resource.load_font("Pacifico.ttf")
+local background = resource.load_image("zoid.jpeg")
 
 function node.render()
+    background:write(0, 0, WIDTH, HEIGHT)
     font:write(120, 320, "Hello World", 100, 1,1,1,1)
 end
