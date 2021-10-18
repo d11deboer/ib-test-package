61e20a72415502ae9ff7e4473dfc9a9da6538635
David D <46694467+d11deboer@users.noreply.github.com>
2021-10-18 11:09:27 -0600

    patching background

--- node.lua
+++ node.lua
@@ -4,6 +4,6 @@ local font = resource.load_font("Pacifico.ttf")
 local background = resource.load_image("zoid.jpeg")
 
 function node.render()
-    background:write(0, 0, WIDTH, HEIGHT)
+    background:draw(0, 0, WIDTH, HEIGHT)
     font:write(120, 320, "Hello World", 100, 1,1,1,1)
 end
