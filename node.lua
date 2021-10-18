1ef24e421b1fccac6be41a9e788c7c96eb6c70e5
David D <46694467+d11deboer@users.noreply.github.com>
2021-10-18 11:00:36 -0600

    Patch font name

--- node.lua
+++ node.lua
@@ -1,6 +1,6 @@
 gl.setup(1024, 768)
 
-local font = resource.load_font("silkscreen.ttf")
+local font = resource.load_font("Pacifico.ttf")
 
 function node.render()
     font:write(120, 320, "Hello World", 100, 1,1,1,1)
